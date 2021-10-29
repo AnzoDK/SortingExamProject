034f2a39f739e43686732ed80bb5be503fe62d91
AnzoDK <antonr@live.dk>
2021-10-29 11:59:38 +0200

    Can now run :)

--- SortingProject/src/main/java/module-info.java
+++ SortingProject/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.test.sortingproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
+    requires javafx.base;
     opens com.test.sortingproject to javafx.fxml;
     exports com.test.sortingproject;
 }
