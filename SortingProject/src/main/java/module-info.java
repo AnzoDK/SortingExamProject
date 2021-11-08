7eb9548e19d77a8410278165e701fc8dbffdc907
AnzoDK <antonr@live.dk>
2021-11-08 13:59:25 +0100

    added primitives for DB and login

--- SortingProject/src/main/java/module-info.java
+++ SortingProject/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.test.sortingproject {
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.base;
+    requires java.sql;
     opens com.test.sortingproject to javafx.fxml;
     exports com.test.sortingproject;
 }
