d31c1764c73a1eacc9b3928b3b75633c3cf69456
Lasermorph <stefanashansen@hotmail.com>
2021-11-15 13:18:31 +0100

    added picture to login

--- SortingProject/src/main/java/com/test/sortingproject/LoginController.java
+++ SortingProject/src/main/java/com/test/sortingproject/LoginController.java
@@ -2,12 +2,15 @@ package com.test.sortingproject;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 public class LoginController {
 
     @FXML
     private Text clkSignUp;
+    @FXML
+    private ImageView JavailPic;
 
     @FXML
     private void switchToSecondary() throws IOException {
